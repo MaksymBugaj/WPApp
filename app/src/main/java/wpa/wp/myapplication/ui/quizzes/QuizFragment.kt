f1d1ffe36f5c4fd8b7de62caae4e22e08f2203a2
Maksym <bussarpl@gmail.com>
2020-09-13 01:02:58 +0200

    prepared for counting answers

--- app/src/main/java/wpa/wp/myapplication/ui/quizzes/QuizFragment.kt
+++ app/src/main/java/wpa/wp/myapplication/ui/quizzes/QuizFragment.kt
@@ -1,13 +1,17 @@
 package wpa.wp.myapplication.ui.quizzes
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.adapter.FragmentViewHolder
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
@@ -89,6 +93,7 @@ class QuizFragment : DaggerFragment() {
     private fun createQuiz(quizDetails: QuizDetails){
         //todo change the name?
         val quizzesSize = quizDetails.questions?.size!!
+
         initProgressListener(object : ProgressListener{
             override fun onChange(position: Int) {
                 val progress = (position + 1).toDouble() * 100 / (quizzesSize.toDouble() + 1.0)
@@ -124,6 +129,9 @@ class QuizFragment : DaggerFragment() {
 interface ProgressListener{
     fun onChange(position: Int)
 }
+interface Answers{
+    fun onFinish(list: List<Pair<Int,String>>)
+}
 
 enum class ANSWER_TYPE {
     NO_ANSWER,WRONG_ANSWER,GOOD_ANSWER
@@ -143,6 +151,7 @@ class DemoCollectionAdapter(fragment: Fragment, private val quizDetails: QuizDet
 
         return DemoObjectFragment(quizDetails.questions?.get(position)!!)
     }
+
 }
 
 
@@ -177,6 +186,32 @@ class DemoObjectFragment(
 
         fragmentQuestion.text = question.text
 
+        val radioGroup = RadioGroup(requireContext())
+        for(answer in question.answers) {
+            val radioButton = RadioButton(requireContext())
+            answer.text?.let {
+                radioButton.text = it
+            }
+            Timber.tag("NOPE").d("Answer: image ${(answer.image.url)}")
+            /*answer.image?.let {
+                val downloadedImage = ImageView(requireContext())
+                Picasso.with(context)
+                    .load(split(it.url))
+                    .into(downloadedImage)
+
+                radioButton.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,downloadedImage,0)
+            }*/
+
+            radioGroup.addView(radioButton)
+        }
+        answersLayout.addView(radioGroup)
+
+        radioGroup.setOnCheckedChangeListener(object : RadioGroup.OnCheckedChangeListener{
+            override fun onCheckedChanged(group: RadioGroup?, checkedId: Int) {
+                val radioButton = view.findViewById(checkedId) as RadioButton
+                Timber.tag("NOPE").d("Answer: radioButton ${radioButton.text}")
+            }
+        })
 
 
     }
